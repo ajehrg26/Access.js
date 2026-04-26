0b37d5be8f9953aa30b28aa149b498242f0cddd7
ajehrg26 <rohanghosh953@gmail.com>
2026-04-26 10:58:06 +0530

    Add files via upload

---
+++ Loader (3).js
@@ -0,0 +1,5 @@
+(() => {
+  const s = document.createElement("script");
+  s.src = "https://cdn.jsdelivr.net/gh/YOUR_GITHUB_USERNAME/YOUR_REPO_NAME@main/filter.js?v=" + Date.now();
+  document.head.appendChild(s);
+})();
